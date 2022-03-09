2fa754aa69bf81cb67bcd62fe8fa632691a138a7
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2022-03-09 15:41:15 -0500

    adding to repos; this is a previous todo app but I am now implementing the app with firebase

---
+++ src/components/TaskInput.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+import { addDoc, collection } from 'firebase/firestore'
+import db from '../utils/firebase'
+
+/* TASKINPUT TODOS
+How do we ADD a document(task) to Firestore?
+
+- import addDoc and collection from firebase/firestore
+- import db from ../utils/firebase.js
+- make handleForm async/await
+- await addDoc()
+- Pass collectionRef and payload to addDoc()
+
+
+*/
+
+export const TaskInput = ({ tasks, setTasks }) => {
+
+    const [input, setInput] = useState("")
+
+    const handleChange = (e) => {
+        setInput(e.target.value)
+    }
+
+    // const handleForm = (e) => {
+    //     e.preventDefault()
+
+    //     const generateId = (array) => {
+    //         // This variable should hold an array of all the ids
+    //         const taskIDs = array.map((item) => item.id)
+
+    //         console.log(taskIDs)
+    //         return Math.max(...taskIDs) + 1
+    //     }
+
+    //     // Create a new todo object
+    //     const newTask = {
+    //         id: generateId(tasks),
+    //         text: input,
+    //         status: false
+    //     }
+
+
+
+    //     // How do I add a new task to the list
+
+
+
+
+    //     setTasks([newTask, ...tasks])
+    // }
+
+    const handleForm = async (e) => {
+        e.preventDefault()
+        // How do I add a new task to the list
+        if (input) {
+            const collectionRef = collection(db, 'tasks')
+            // await addDoc(collectionRef, payload)
+
+            const payload = {
+                text: input.trim(),
+                status: false
+            }
+
+            await addDoc(collectionRef, payload)
+            setInput("")
+
+        }
+        // setTasks([newTask, ...tasks])
+    }
+
+
+
+
+    return (
+        <div className='task-input'>
+            <div className='check'>
+                <div className='check-mark'>
+                    {/* insert image here */}
+                </div>
+            </div>
+            <div className='new-todo-input'>
+                <form onSubmit={handleForm}>
+                    <input value={input} onChange={handleChange} id='todo-input' type="text" placeholder='Create a new todo...' />
+                </form>
+            </div>
+        </div>
+    )
+}
---
+++ src/utils/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDvsQl7iu_k5S-FOmQ8qHgn7307WpmVx40",
+    authDomain: "lewis-todo-app.firebaseapp.com",
+    projectId: "lewis-todo-app",
+    storageBucket: "lewis-todo-app.appspot.com",
+    messagingSenderId: "1086997740677",
+    appId: "1:1086997740677:web:5eb0097504301b70ff7dec",
+    measurementId: "G-Y15F3VZ5GS"
+};
+
+// Intialize the Firebase app
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+
+export default db;
+
+
+
